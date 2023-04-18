eabc4c0d4949ac193ad313d0186a23fa199ac4cc
Kurtmj93 <kurtmj93@gmail.com>
2023-04-18 13:54:53 -0400

    finish resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,9 +4,13 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    me: async () => {
-      const userMe = await User.find({});
-      return userMe;
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userMe = await User.findOne({_id: context.user._id}).select('-__v -password');
+        return userMe;
+      } else {
+        throw new AuthenticationError('You need to be logged-in!');
+      }
     }
   },
   Mutation: {
@@ -30,12 +34,33 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
-    saveBook: async (parent, {bookInfo}) => {
+    saveBook: async (parent, {bookInfo}, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: bookInfo }},
+          { new: true }
+        );
+        
+        return updatedUser;
+      } else {
+        throw new AuthenticationError('You need to be logged-in!');
+      }
     },
-    removeBook: async (parent, {bookId}) => {
+    removeBook: async (parent, {bookId}, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId }}},
+          { new: true }
+        );
 
+        return updatedUser;
+      } else {
+        throw new AuthenticationError('You need to be logged-in!')
+      }
     }
-  },
+  }
 };
 
 module.exports = resolvers;
