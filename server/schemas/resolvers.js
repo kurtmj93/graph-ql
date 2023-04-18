4a1bdd967434f9643168b251e678d54ea3f90152
Kurtmj93 <kurtmj93@gmail.com>
2023-04-18 12:27:11 -0400

    write login and addUser mutations, add tokens from user-controller

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -9,7 +9,19 @@ const resolvers = {
   },
   Mutation: {
     login: async (parent, { email, password }) => {
+
       const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError('No user with that email.');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password.');
+      }
+
+      const token = signToken(user);
+      return { token, user };
     },
     addUser: async (parent, {username, email, password}) => {
       const user = await User.create({username, email, password});
