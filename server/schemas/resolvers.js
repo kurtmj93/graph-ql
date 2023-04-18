0c5f19490241f3e1386fec61067a856a4262a997
Kurtmj93 <kurtmj93@gmail.com>
2023-04-18 12:28:24 -0400

    require signToken from auth.js in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { Book, User } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
