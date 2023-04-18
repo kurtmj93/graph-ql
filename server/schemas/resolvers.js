bfa0470c76a892ed1b8c75dc2a8e170810029484
Kurtmj93 <kurtmj93@gmail.com>
2023-04-18 12:14:28 -0400

    write TypeDefs xMutations, basic syntax and required args in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,10 +2,24 @@ const { Book, User } = require('../models');
 
 const resolvers = {
   Query: {
-    
+    me: async () => {
+      const userMe = await User.find({});
+      return userMe;
+    }
   },
   Mutation: {
-    
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+    },
+    addUser: async (parent, {username, email, password}) => {
+      const user = await User.create({username, email, password});
+      return user;
+    },
+    saveBook: async (parent, {}) => {
+    },
+    removeBook: async (parent, {bookId}) => {
+
+    }
   },
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,35 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Mutation {
+
+  }
 `;
 
 module.exports = typeDefs;
